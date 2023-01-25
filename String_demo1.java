b90b59edad59442ecc1c220cf6f157d0f4f5422f
Pranali Sahare <pranalisahare678@gmail.com>
2023-01-25 12:38:10 +0530

    String_demoexample

---
+++ String_demo1.java
@@ -0,0 +1,15 @@
+public class Stringdemo1{
+    public static void main(String[] args){
+       string s ="pranali";
+
+      char ch;
+      for(int i=0;i<7;i++){
+       ch=s.charAt(i);
+       System.out.println(ch);
+
+      }
+
+
+}
+
+}
