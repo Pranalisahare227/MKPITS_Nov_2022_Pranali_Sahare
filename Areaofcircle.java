97d3d5bdd62dd1f4ab7bcaf72ec895e96ca902da
Pranali Sahare <pranalisahare678@gmail.com>
2023-01-27 10:48:38 +0530

    Area of circle in java

---
+++ Areaofcircle.java
@@ -0,0 +1,13 @@
+public class Areaofcircle{
+     public static void main(String args[])
+     {
+
+     double pi=3.14, r=2.5,area;
+     area=pi*r*r;
+
+     System.out.println("Area of Circle is " );
+     System.out.println(area);
+
+     }
+}
+
