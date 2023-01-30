ffe9db9b66ea875a70cdaecf5acd93bb0f746142
Pranali Sahare <pranalisahare678@gmail.com>
2023-01-30 17:38:50 +0530

    Array_largestnum_demo

---
+++ 1assig_array_largest_num_25jan.java
@@ -0,0 +1,63 @@
+
+public class array {
+	public static void main(String[] args) {
+		int arrSize = 3;
+		int[] nums1 = {10, 20, -30};
+		System.out.print("Elements in original array are: "+nums1);
+
+		int[] nums2 = {10, 20, 30};
+		System.out.print("Elements in original array are: "+nums2);
+
+
+		int[] res = new int[arrSize];
+
+		for(int i = 0; i < arrSize; i++)
+
+	        {
+
+		System.out.println("The array which has the largest sum.: ");
+
+	        }
+
+	        public class assig1_largest_num_25jan
+
+	        {
+	           public static void main (String[] args){
+
+	           array ob = new array();
+
+
+
+
+
+	           }
+
+
+
+	        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
