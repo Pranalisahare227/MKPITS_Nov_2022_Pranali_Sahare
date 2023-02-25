bc25adedded569942a0ab63b68de48e5992b72ca
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-26 00:17:05 +0530

    Ifelse_demo

---
+++ IfElseExample1.java
@@ -0,0 +1,20 @@
+public class IfElseExample1 {
+     public static void main(String args[]) {
+     int x=30;
+
+     if(x<20) {
+        System.out.println(x);
+
+        System.out.println("This is if statement");
+        }else{
+
+        System.out.println(x);
+
+        System.out.println("This is else statement");
+          }
+
+
+     }
+
+
+}
