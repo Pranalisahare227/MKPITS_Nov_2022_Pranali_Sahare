80d6dc0697f8e6bd5ce00f7e8d74cebb5e2f93f9
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-26 00:18:17 +0530

    demo_ex

---
+++ IfElseExample2.java
@@ -0,0 +1,18 @@
+public class IfElseExample2 {
+     public static void main(String args[]) {
+     int age=19;
+
+     if(age==18) {
+
+        System.out.println("Yes boy you can drive!");
+        } else{
+
+        System.out.println("No boy you cannot drive yet!");
+          }
+
+
+     }
+
+
+}
+
