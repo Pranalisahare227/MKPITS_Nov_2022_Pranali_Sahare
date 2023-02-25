87338f1fb0b0f94e9709c822eacd2b081fce06b1
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-26 00:26:18 +0530

    IF_DEMOEX

---
+++ IfExample1.java
@@ -0,0 +1,17 @@
+
+
+public class IfExample1 {
+     public static void main(String args[]) {
+     int x=10;
+
+     if(x<20) {
+        System.out.println("The value of x");
+        System.out.println(x);
+        System.out.println("This is if statement");
+        }
+
+
+     }
+
+
+}
