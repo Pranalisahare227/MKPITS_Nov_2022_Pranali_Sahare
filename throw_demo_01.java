55a0122d50c50d6e6480bebdff97cc05d50b5175
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-26 00:56:09 +0530

    thro_demo

---
+++ throw_demo_01.java
@@ -0,0 +1,11 @@
+public class throw_demo_01 {
+  public static void main(String[] args) throws Exception
+  {
+
+  System.out.println("Start Program");
+  Thread.sleep(100);
+  System.out.println("End Program");
+
+
+  }
+}
