fe4411fb2e2f90fe74c6a10e59d0df9b7fa4f896
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-26 00:27:57 +0530

    IfEX_DEMO

---
+++ IfExample2.java
@@ -0,0 +1,16 @@
+public class IfExample2 {
+     public static void main(String args[]) {
+     int b=10;
+
+     if(b>0) {
+
+        System.out.println("number is positive");
+        }
+        System.out.println("Statement outside if block");
+
+
+
+     }
+
+
+}
