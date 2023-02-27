9475e6789b4ff01847913fd7bfd7f5a340d1d4e9
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-27 05:58:38 +0530

    user_input_demo

---
+++ user_input.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class user_input{
+
+   public static void main(String[] args){
+
+   int a, b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter  number");
+    a=sc.nextInt();
+    b=sc.nextInt();
+
+    System.out.println(a+" "+b);
+
+ }
+
+}
