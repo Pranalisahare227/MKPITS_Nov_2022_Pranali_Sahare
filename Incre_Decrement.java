86b7a839c3a3f73e296f4627c83c508c7fa024cb
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-27 05:54:29 +0530

    In/Decrement_demo

---
+++ Incre_Decrement.java
@@ -0,0 +1,19 @@
+//Incre/Decrement Operator
+
+class Incre_Decrement {
+
+  public static void main(String[] args){
+
+    int a=11;
+    System.out.println(a);
+    System.out.println("Prepost Increment");
+    System.out.println(a++);
+    System.out.println(++a);
+
+    System.out.println("Prepost Decrement");
+	System.out.println(a--);
+	System.out.println(--a);
+
+  }
+
+}
