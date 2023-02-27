acf316ad51fadac99ce7c7a02f9c1b1feceb11da
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-27 05:53:06 +0530

    Assignment_operator_Demo

---
+++ Assignmennt_opr.java
@@ -0,0 +1,20 @@
+//Assignment Operator
+
+class Assignmennt_opr{
+
+ public static void main(String[] args){
+   int a=12;
+    System.out.println(a);  //SIMPLE
+
+
+    a+=12;   //a=a+12  compound
+   System.out.println(a);
+
+
+
+       a-=12;   //a=a-12  compound
+      System.out.println(a);
+
+ }
+
+}
