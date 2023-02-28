d31e63d3c176597e0ce0371f59d8821b9552b758
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-28 07:32:45 +0530

    Even_odd_demo

---
+++ even_odd.java
@@ -0,0 +1,29 @@
+ //Even or odd numbers
+ 
+ import java.util.Scanner;
+ 
+ class even_odd{
+ 
+ public static void main(String[] args){
+ 
+ int a;
+ 
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Enter number");
+ a=sc.nextInt();
+
+if(a%2==0){
+
+System.out.println("Number is even");
+
+}
+
+else{
+
+
+System.out.println("Number is odd");
+
+}
+ 
+ }
+   }
