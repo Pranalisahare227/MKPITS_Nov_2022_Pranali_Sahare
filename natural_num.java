2f247c2d8a4ba3689f0e81811fcbcdcded90115c
Pranali Sahare <pranalisahare678@gmail.com>
2023-02-28 07:34:20 +0530

    Natural_num_demo

---
+++ natural_num.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+public class natural_num{
+
+   public static void main(String[] args){
+
+   int n;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter num of term");
+   n=sc.nextInt();
+
+   for(int i=1;i<=n;i++)
+   {
+
+	System.out.print(" " +i);
+
+	}
+
+
+
+   }
+
+}
