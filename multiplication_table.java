d9d0e51f47ea4595eab93ee059f5f8422265dd29
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-03 06:59:34 +0530

    mul_table

---
+++ multiplication_table.java
@@ -0,0 +1,23 @@
+//multiplication table in java
+
+
+public class multiplication_table{
+
+	public static void main (String[] args){
+
+		int num=2;
+
+		for(int i=1;i<=10;i++){
+
+			System.out.println(num+"*"+i+"="+num*i);
+
+
+			}
+
+		}
+
+
+
+
+
+	}
