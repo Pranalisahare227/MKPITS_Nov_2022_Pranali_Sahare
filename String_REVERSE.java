821c5b09a9bb561bcf1291ad1c5c9f2221540e09
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-03 07:01:10 +0530

    REVERSE_string_ex

---
+++ String_REVERSE.java
@@ -0,0 +1,31 @@
+/*String reverse*/
+
+class String_REVERSE
+{
+
+public static void main(String[] args){
+
+int l,i;
+String r="hello Beautiful";
+String r2="";
+l=r.length();
+
+
+
+for(i=l-1;i>=0;i--){
+
+
+r2=r2+r.charAt(i);
+
+}
+
+System.out.println(r2);
+
+
+
+
+
+
+}
+
+}
