f125e903e1a6c2b18612ab1756b8a94a466872a6
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-03 06:51:55 +0530

    even_range_ex

---
+++ evennum_range.java
@@ -0,0 +1,26 @@
+/*Print even numbers in Given range*/
+
+import java.util.Scanner;
+
+class evennum_range{
+
+         public static void main(String[] args){
+
+         int n;
+
+         System.out.println("Enter the number of term");
+         Scanner sc = new Scanner(System.in);
+         n=sc.nextInt();
+
+         for(int i=0; i<=n; i=i+2){
+
+         System.out.print(i+ " ");
+
+    }
+
+    }
+
+  }
+
+
+
