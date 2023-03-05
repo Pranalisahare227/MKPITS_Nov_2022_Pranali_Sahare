ed8191b7fc7365f06ddd6447f145ff1725849b44
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-05 20:32:15 +0530

    String_Comparison_demo

---
+++ String_comparison.java
@@ -0,0 +1,22 @@
+/* String Comparison in Java*/
+
+class String_comparison{
+     public static void main(String[] args){
+
+     String a="Tejas";
+     String b=new String("Tejas");  //new Keyword
+
+     if(a.equals(b)){
+
+     System.out.println("True");
+
+     }else{
+
+     System.out.println("False");
+
+
+     }
+
+     }
+
+}
