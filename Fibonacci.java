4e61e595bab610b9b6edaea5dc9362290fb4cf65
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-05 20:26:24 +0530

    Fibonacci_demo

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+/* Program to print Fibonacci series */
+
+
+import java.util.Scanner;
+class Fibonacci{
+  public static void main(String[] args){
+    int term,a=0,b=1,c;
+    System.out.print("Enter term ");
+    Scanner sc=new Scanner(System.in);
+    term=sc.nextInt();
+
+    for(int i=1;i<=term;i++){
+
+		System.out.print(a+" ");
+
+        c=a+b;
+        a=b;
+        b=c;
+    }
+
+  }
+
+}
