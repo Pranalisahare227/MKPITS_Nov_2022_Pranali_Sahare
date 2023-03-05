20053a1fa57beb473f815c8548b28bcdaf6adb35
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-05 20:28:18 +0530

    geometric_series

---
+++ geometric.java
@@ -0,0 +1,23 @@
+class geo
+{
+
+int i;
+void set(){
+
+for(i=5;i<=300;i=i*2)
+{
+
+System.out.println(i);
+
+  }
+ }
+}
+ class geometric{
+   public static void main(String[] args)
+   {
+   
+   geo ob=new geo();
+   ob.set();
+   }
+ 
+}
