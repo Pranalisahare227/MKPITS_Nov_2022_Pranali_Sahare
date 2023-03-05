3a309f1ff4a27f58a2726a3f6cd5dfb252192fd8
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-05 20:15:10 +0530

    Arithmetic_series_

---
+++ arithmetic_series.java
@@ -0,0 +1,24 @@
+class arith{
+
+  int i;
+  void set(){
+
+  for(int i=1;i<=30;i=i+3)
+  {
+
+  System.out.println(i);
+
+  }
+   }
+}
+
+
+public class arithmetic_series{
+
+    public static void main(String[] args){
+
+    arith ob =new arith();
+    ob.set();
+
+    }
+}
