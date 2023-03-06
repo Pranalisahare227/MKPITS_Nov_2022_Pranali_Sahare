0f42583bbe51c4fee20d78cc93732a781be84952
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-06 14:44:30 +0530

    Assign_que_Prime

---
+++ Prime_num_assig7.java
@@ -0,0 +1,21 @@
+/*Prints out the prime numbers between 1 and 200.*/
+class Prime_num_assig7{
+
+public static void main(String [] args){
+
+int i;
+
+System.out.println(2);
+
+for(i=3;i<=200;i++){
+
+	if(i%2==1){
+
+	System.out.println(""+i);
+
+         }
+	}
+
+}
+
+}
