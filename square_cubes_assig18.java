8a5831b5d99c5c574216578b0a88159b97c5657b
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-06 14:50:03 +0530

    Square_Cube_assign_Que

---
+++ square_cubes_assig18.java
@@ -0,0 +1,25 @@
+/*18.Write a Java programming to calculate and prints the squares and cubes of the numbers from 0 to 20 and uses tabs to display them in a table of values.*/
+
+class square_cubes_assig18{
+
+public static void main(String [] argus){
+
+
+int i;
+
+for(i=0;i<=20;i++){
+
+int sq,cu;
+
+sq = i*i;
+
+cu = i*i*i;
+
+	//System.out.println(" "+i+" "+sq+" "+cu+" ");
+
+		System.out.println(+i+" "+cu+" "+sq+" ");
+
+	}
+}
+
+}
