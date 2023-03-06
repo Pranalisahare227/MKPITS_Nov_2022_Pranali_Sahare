ace0ad9c967bce8acfbe9ce80dee2c129e5befbf
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-06 22:22:38 +0530

    String_check_ex

---
+++ String_given_check.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class String_given_check{
+
+public static void main(String [] argu){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the first name");
+String a = sc.nextLine();
+
+System.out.println("Enter second name");
+String b = sc.nextLine();
+
+if(a == b){
+
+	System.out.println("True");
+	}
+
+	else{
+
+	System.out.println("False");
+
+	}
+}
+}
