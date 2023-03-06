364317b3a78eec875645f73addee4d8d662a08c3
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-06 14:33:09 +0530

    Basic_prog

---
+++ Java_Basic_PROG5.java
@@ -0,0 +1,15 @@
+public class Java_Basic_PROG5 {
+
+    public static void my_method(String fname , int x){
+
+        System.out.println(fname +" is "+ x);
+
+    }
+    public static void main(String[] args) {
+        my_method("Pranu",22);
+
+        my_method("Pranu",22);
+
+        my_method("Pranu",22);
+    }
+}
