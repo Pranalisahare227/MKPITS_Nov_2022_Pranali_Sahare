497730e1a18921d6b433e151ba5b39a8fc77d58e
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-06 14:34:09 +0530

    Basic_prog

---
+++ Java_Basics_Prog1.java
@@ -0,0 +1,13 @@
+
+public class Java_Basics_Prog1 {
+
+public static int my_method(int x, int y){
+
+    return x+y;
+
+}
+    public static void main(String[] args) {
+     int sum =  my_method(5,3);
+        System.out.println(sum);
+    }
+}
