bee70fc2b160acdbedacf4c7ea6d8e8411b01197
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-07 07:59:35 +0530

    _MultiplySequence_PATTERN

---
+++ Pattern_Sequence.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class Pattern_Sequence{
+  public static void main(String[] args){
+
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter value of n");
+ int n=sc.nextInt();
+
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=i;j++)
+
+    {
+
+    System.out.println("*");
+
+    }
+
+    System.out.println(" ");
+
+  }
+
+  }
+
+}
