9173c7b61893cad34b0c7a213c2c8e7beecf671d
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-07 07:58:18 +0530

    _Multi_Pattern_demo

---
+++ multi_Pattern.java
@@ -0,0 +1,27 @@
+
+
+class multi_Pattern{
+  public static void main(String[] args){
+
+  int i,j;
+
+  System.out.println("Enter value of 5");
+
+
+  for(i=1;i<=5;i++)
+  {
+    for(j=1;j<=5;j++)
+
+    {
+
+    System.out.println("*");
+
+    }
+
+    System.out.println(" ");
+
+  }
+
+  }
+
+}
