cf2ab6eefa17d6511bd39d99058b4b22be565ae3
Pranali Sahare <pranalisahare678@gmail.com>
2023-03-10 23:57:35 +0530

    Remove_whte_space

---
+++ Remove_Whitespaces.java
@@ -0,0 +1,12 @@
+/* Program to Remove All Whitespaces*/
+
+public class Remove_Whitespaces {
+
+    public static void main(String[] args) {
+        String sentence = "T    his is b  ett     er.";
+        System.out.println("Original sentence: " + sentence);
+
+        sentence = sentence.replaceAll("\\s", "");
+        System.out.println("After replacement: " + sentence);
+    }
+}
