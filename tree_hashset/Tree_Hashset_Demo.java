9ffaf100abc2d8e6acf086116911b8aff261cf9e
Pranali Sahare <pranalisahare678@gmail.com>
2023-05-30 17:26:32 +0530

    Treehashset_Prog

---
+++ tree_hashset/Tree_Hashset_Demo.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+public class Tree_Hashset_Demo {
+    public static void main(String[] args){
+
+        TreeSet ts=new TreeSet();
+        ts.add(2);
+        ts.add(12);
+        ts.add(6);
+        ts.add(6);
+
+        System.out.println(ts);
+    }
+}
